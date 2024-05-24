acb113a44d2f762ff43a33f8f7031ba772feedc7
sebsto93 <s.stobiecki93@gmail.com>
2024-05-24 18:35:34 +0200

    task-2.done

--- task-1.js
+++ task-1.js
@@ -40,6 +40,6 @@ console.log(
       balance: 2764
     },
   ])
-  ); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+  ); 
   
   
